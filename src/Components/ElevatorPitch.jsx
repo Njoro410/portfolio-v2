a17b810facaf7a136fb01ece676fff26d410d960
njoro410 <brian.njoroge@student.moringaschool.com>
2023-08-21 01:08:42 +0300

    added ElevatorPitch video

--- src/Components/ElevatorPitch.jsx
+++ src/Components/ElevatorPitch.jsx
@@ -34,11 +34,34 @@ const ElevatorPitch = () => {
       name="elevator-pitch"
       className="relative flex flex-wrap lg:h-screen lg:items-center bg-red-400/10"
     >
-      <p className="font-Londrina text-5xl text-center w-full"> Elevator Pitch</p>
+      <p className="font-Londrina text-5xl text-center w-full">
+        {" "}
+        Elevator Pitch
+      </p>
 
-      <div className="w-full px-4 py-12 lg:w-1/2 sm:px-6 lg:px-8 sm:py-16 lg:py-24"></div>
-
-      <div className="relative w-full h-64 sm:h-96 lg:w-1/2 lg:h-full"></div>
+      <div className="w-full mb-24">
+        <div className="flex justify-center items-center flex-col">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/sX2zBMwe-uU"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+          <p className="text-xl md:text-3xl font-extrabold text-center font-Dosis ">
+            <a
+              target="_blank"
+              className="hover:underline"
+              rel="noreferrer"
+              href="https://www.youtube.com/embed/sX2zBMwe-uU"
+            >
+              Click to view
+            </a>
+          </p>
+        </div>
+      </div>
     </motion.section>
   );
 };
